593e57c3707d64d94ac5d485d44fd678526b0cc5
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-06-23 21:13:52 +0900

    Add user model and database connection setup

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
